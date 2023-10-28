2a4618b4bf4fede0352f9bbe62d8f4c447a046d5
malaygathani <145202865+malaygathani@users.noreply.github.com>
2023-10-28 17:35:29 +0530

    Game.js

--- components/Game.js
+++ components/Game.js
@@ -1,1 +1,15 @@
-
+AFRAME . registerComponet("game-play",{
+    schema :{
+        elementld:{type:"sttring",defalut:"#ring1"},
+    },
+    isCollided:function(elementld){
+        const element = document.querySelector(elementld);
+        element.addEventListener("collide",(e)=>{
+            if(elementld.includes("#ring")){
+                console.log(elementld +"collision");
+            }else if (elementld.includes("#hurdle")){
+                console.log("bird collision");
+            }
+        });
+    },
+});
